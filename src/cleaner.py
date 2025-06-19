be7339d242d4606e517a832bd56e1858b4b35585
o-lebedes <lebedes98@gmail.com>
2025-06-19 15:58:32 +0800

    feat: реализована базовая очистка таблицы

--- src/cleaner.py
+++ src/cleaner.py
@@ -11,9 +11,21 @@ class TabelCleaner:
         self.filepath = filepath
         self.df = pd.read_excel(filepath)
 
-    def clean(self):
-        # TODO: реализовать предварительную очистку (например, удаление пустых строк)
-        pass
+def clean(self):
+    # Удаляем полностью пустые строки (где вообще нет данных в строке)
+    self.df.dropna(how='all', inplace=True)
+
+    # Удаляем строки, где нет значений в первом столбце (например, нет ФИО или табельного номера)
+    self.df.dropna(subset=[self.df.columns[0]], inplace=True)
+
+    # Очищаем все ячейки от лишних пробелов в начале и в конце (если это строка)
+    # Например, "  Я11 " -> "Я11"
+    self.df = self.df.applymap(
+        lambda x: x.strip() if isinstance(x, str) else x
+    )
+
+    # Печатаем в консоль сообщение — чтобы понимать, что метод отработал
+    print("Очистка завершена.")
 
     def detect_anomalies(self):
         # TODO: реализовать выявление отклонений (например, короткие смены, Я11 вместо ВМ11 и пр.)
